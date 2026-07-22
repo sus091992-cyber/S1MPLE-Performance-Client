42ec2744893e4c79d62bce1b7f1fbcb94a608e9b
s1mple <s1mple@localhost>
2026-07-22 20:33:29 +0330

    fix: revert to simple plugin block

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,6 @@
-buildscript {
-    repositories {
-        mavenCentral()
-        maven("https://maven.fabricmc.net/")
-        gradlePluginPortal()
-    }
-    dependencies {
-        classpath("net.fabricmc:fabric-loom:1.3")
-    }
+plugins {
+    id("fabric-loom") version("1.3")
 }
-
-apply(plugin = "fabric-loom")
 
 version = "1.0.0"
 group = "com.s1mple.performance"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,8 @@
+pluginManagement {
+    repositories {
+        maven { url = uri("https://maven.fabricmc.net/") }
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+        gradlePluginPortal()
+    }
+}
 rootProject.name = "S1MPLE-Performance-Client"
