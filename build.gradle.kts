15482fdf56b4f94cb895756777b46a24884b7fb3
s1mple <s1mple@localhost>
2026-07-23 03:59:13 +0330

    fix: use buildscript with loom 1.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,15 @@
-plugins {
-    id("fabric-loom") version("1.4")
+buildscript {
+    repositories {
+        mavenCentral()
+        maven("https://maven.fabricmc.net/")
+        maven("https://plugins.gradle.org/m2/")
+    }
+    dependencies {
+        classpath("net.fabricmc:fabric-loom:1.4")
+    }
 }
+
+apply(plugin = "fabric-loom")
 
 version = "1.0.0"
 group = "com.s1mple.performance"
