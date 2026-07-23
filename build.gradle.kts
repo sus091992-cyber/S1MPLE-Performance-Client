2006653943c5349ee1c0c841feefceb65d3d6a38
s1mple <s1mple@localhost>
2026-07-23 05:00:23 +0330

    fix: use plugin block with loom 1.8.0 + resolutionStrategy in settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,6 @@
-buildscript {
-    repositories {
-        mavenCentral()
-        maven("https://maven.fabricmc.net/")
-        maven("https://plugins.gradle.org/m2/")
-    }
-    dependencies {
-        classpath("net.fabricmc:fabric-loom:1.4.1")
-    }
+plugins {
+    id("fabric-loom") version("1.8.0")
 }
-
-apply(plugin = "fabric-loom")
 
 version = "1.0.0"
 group = "com.s1mple.performance"
@@ -31,14 +22,6 @@ dependencies {
     modCompileOnly("net.caffeinemc.sodium:sodium-fabric:0.6.0+mc1.21.4")
 }
 
-loom {
-    runConfigs {
-        "client" {
-            ideConfigGenerated(true)
-        }
-    }
-}
-
 processResources {
     inputs.property("version", project.version)
     inputs.property("fabricid", "s1mple-performance")
@@ -50,10 +33,4 @@ processResources {
 
 tasks.jar {
     archiveFileName.set("S1MPLEPerformance-Client-${project.version}.jar")
-}
-
-tasks.register("printJarPath") {
-    doLast {
-        println(tasks.jar.get().archiveFile.get().asFile.absolutePath)
-    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,15 @@
 pluginManagement {
     repositories {
+        mavenCentral()
         maven { url = uri("https://maven.fabricmc.net/") }
-        maven { url = uri("https://plugins.gradle.org/m2/") }
         gradlePluginPortal()
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "fabric-loom") {
+                useModule("net.fabricmc:fabric-loom:1.8.0")
+            }
+        }
     }
 }
 rootProject.name = "S1MPLE-Performance-Client"
